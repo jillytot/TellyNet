4f1d5a6b48dcdea66e2f3cc32e878acb948534ae
Jacqui Manzi <jacquimanzi@gmail.com>
2016-10-29 18:03:48 -0700

    ENG-4 - Using websockets instead of socket.io to communicate with C#.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
+/* eslint new-cap: 0 */
+/* eslint padded-blocks: 0 */
+/* eslint brace-style: 0 */
 
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const WebSocketServer = require('websocket').server;
+
 const config = require('../config/config');
 const logger = require('../logs/log');
 
@@ -33,11 +37,49 @@ const server = http.listen(config.get('port'), () => {
   logger.log('info', 'TellyNet activated http://%s:%s', host, port);
 
   Twitch.setupConnection(config.get('twitch').events);
+
 });
 
 /**
- *  Initiate Socket IO, listen for socket connections
+ *  Web Sockets Section
  */
-io.on('connection', function(socket){
-  console.log('a user connected');
+
+const wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  console.log(origin);
+  //@TODO: Set the origin of the web socket and verifiy here
+  //return config.get('allowedOrigins').indexOf(origin) !== -1;
+  return true;
+}
+
+wsServer.on('request', request => {
+
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    logger.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    return;
+  }
+
+  const connection = request.accept(undefined, request.origin);
+
+  logger.log(new Date() + ' Connection accepted.');
+  connection.on('message', message => {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ' + message.utf8Data);
+      connection.sendUTF(message.utf8Data);
+    }
+    else if (message.type === 'binary') {
+      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+      connection.sendBytes(message.binaryData);
+    }
+  });
+
+  connection.on('close', () => {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
 });
