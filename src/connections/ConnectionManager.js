341161a3db3e99e7b38616d2a2f0efb6c9b280a5
Jacqui Manzi <jacquimanzi@gmail.com>
2016-11-25 18:22:04 -0800

    ENG-6 - Removal of connections when disconnected.

--- src/connections/ConnectionManager.js
+++ src/connections/ConnectionManager.js
@@ -2,7 +2,7 @@
 /* eslint padded-blocks: 0 */
 /* eslint brace-style: 0 */
 const WebSocketServer = require('websocket').server;
-const logger = require('../../logs/log');
+const logger = require('winston');
 
 /**
  * ConnectionManager - Web Sockets client manager.
@@ -23,10 +23,11 @@ function ConnectionManager(server) {
 }
 
 ConnectionManager.prototype.setupListeners = function setupListeners() {
-
   this.Server.on('request', request => {
+
     if (!originIsAllowed(request.origin)) {
       // Make sure we only accept requests from an allowed origin
+      logger.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
       request.reject();
       logger.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
       return;
@@ -36,7 +37,9 @@ ConnectionManager.prototype.setupListeners = function setupListeners() {
     this.socketConnections.push(connection);
 
     logger.log(new Date() + ' Connection accepted.');
+    console.log('connection accepted');
     connection.on('message', message => {
+      console.log(message);
       if (message.type === 'utf8') {
         connection.sendUTF(message.utf8Data);
       }
@@ -46,6 +49,12 @@ ConnectionManager.prototype.setupListeners = function setupListeners() {
     });
 
     connection.on('close', () => {
+
+      const index = this.socketConnections.indexOf(connection);
+      if (index > -1) {
+        this.socketConnections.splice(index, 1);
+      }
+
       logger.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
   });
@@ -55,7 +64,8 @@ ConnectionManager.prototype.setupListeners = function setupListeners() {
 function originIsAllowed(origin) {
   // @TODO: Set the origin of the web socket and verify here
   // return config.get('allowedOrigins').indexOf(origin) !== -1;
-  return origin !== undefined;
+  //return origin !== undefined;
+  return true;
 }
 
 module.exports = ConnectionManager;
